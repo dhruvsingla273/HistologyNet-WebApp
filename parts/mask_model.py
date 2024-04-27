10d687d0b8283793bd1e27caef120a4adf513569
ParasBedi298 <b21161@students.iitmandi.ac.in>
2024-04-27 22:50:53 +0530

    Initial Commit

---
+++ parts/mask_model.py
@@ -0,0 +1,57 @@
+import ternausnet
+import ternausnet.models
+import streamlit as st
+import numpy as np
+from PIL import Image
+import io
+
+import torch
+# from torch import nn
+from torchvision import transforms
+from torch.optim import Adam
+
+
+def load_model(path, device='cpu'): # Check for GPU or CPU
+    model = ternausnet.models.UNet11() 
+    # optimizer = Adam(model.parameters(), lr=0.001)
+    checkpoint = torch.load(path, map_location=torch.device(device))
+    model.load_state_dict(checkpoint['model_state_dict'])
+    # optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+
+    model = model.to(device)
+
+    return model
+
+
+def get_masked_image(uploaded_file, model, device='cpu'): # Check for GPU or CPU
+    if st.session_state.edited == False:
+        image_data = uploaded_file.read()
+        input_image = Image.open(io.BytesIO(image_data))
+    else:
+        input_image = uploaded_file
+    if input_image.mode != 'RGB':
+        input_image = Image.merge("RGB", (input_image, input_image, input_image))
+    transform = transforms.Compose([
+        transforms.Resize((256, 256)),
+        transforms.ToTensor()  # resizing may be needed
+    ])
+    input_tensor = transform(input_image).unsqueeze(0).to(device)
+
+    model.eval()  
+    with torch.no_grad():
+        output_tensor = model(input_tensor)
+        output_tensor = torch.sigmoid(output_tensor)
+    output_numpy = output_tensor.squeeze().cpu().numpy() # Check for GPU or CPU
+    output_numpy = (output_numpy > 0.5).astype(np.uint8)
+    output_image = Image.fromarray(output_numpy * 255)
+
+    return output_image
+
+
+def mask_images(uploaded_files, checkpoint_path, device):
+    segmented_files = []
+    model = load_model(checkpoint_path, device)
+    for uploaded_file in uploaded_files:
+        segmented_file = get_masked_image(uploaded_file, model, device)
+        segmented_files.append(segmented_file)
+    return segmented_files
---
+++ parts/submitted.py
@@ -0,0 +1,158 @@
+import streamlit as st
+from streamlit_image_comparison import image_comparison
+from PIL import Image
+import io
+import copy
+from parts.mask_model import mask_images
+import zipfile
+
+def submitted_uploads_page():
+
+    if "segmented_files" not in st.session_state:
+        st.session_state.segmented_files = None
+        st.rerun()
+
+    model_path = "model/model_try2_14.pth"
+    device = "cpu"
+    uploaded_files_copy = [copy.copy(uploaded_file) for uploaded_file in st.session_state.uploaded_files]
+    st.session_state.segmented_files = mask_images(uploaded_files_copy, model_path, device)
+    del uploaded_files_copy
+
+    btn1, btn2, _, btn3 = st.columns(4)
+    
+    if btn1.button("Upload New Files"):
+        st.session_state.page = "basic_uploads"
+        st.session_state.uploaded_files = None
+        st.rerun()
+
+    if btn2.button("Visualize Better"):
+        st.session_state.page = "compare_uploads"
+        st.rerun()
+
+    with btn3:
+        uploaded_files_copy = [copy.copy(uploaded_file) for uploaded_file in st.session_state.uploaded_files]
+        segmented_files_copy = [copy.copy(segmented_file) for segmented_file in st.session_state.segmented_files]
+        download_images(uploaded_files_copy, segmented_files_copy, st.session_state.names)
+        del uploaded_files_copy
+        del segmented_files_copy
+
+    st.write("Segmentation Results: ")
+    st.write("")
+
+    cols = st.columns(2)
+    with cols[0]:
+        st.markdown("<p style='text-align: center;'>Original Image</p>", unsafe_allow_html=True)
+    with cols[1]:
+        st.markdown("<p style='text-align: center;'>Segmentation</p>", unsafe_allow_html=True)
+
+    uploaded_files_copy = [copy.copy(uploaded_file) for uploaded_file in st.session_state.uploaded_files]
+    segmented_files_copy = [copy.copy(segmented_file) for segmented_file in st.session_state.segmented_files]
+    for i in range(len(st.session_state.uploaded_files)):
+        try:
+            with cols[0]:
+                if st.session_state.edited == False:
+                    image_data = uploaded_files_copy[i].read()
+                    input_image = Image.open(io.BytesIO(image_data))
+                else:
+                    input_image = uploaded_files_copy[i]
+                st.image(input_image, use_column_width=True)
+        except Exception as e:
+            st.error(f"Error processing {st.session_state.uploaded_files[i].name}: {e}")
+
+        try:
+            with cols[1]:
+                mask = segmented_files_copy[i]
+                st.image(mask, use_column_width=True)
+        except Exception as e:
+            st.error(f"Error processing Segmentation Mask: {e}")
+    del uploaded_files_copy
+    del segmented_files_copy
+
+
+def download_images(images, masks, names):
+    if st.session_state.edited == False:
+        for i in range(len(images)):
+            image_data = images[i].read()
+            images[i] = Image.open(io.BytesIO(image_data))
+    zip_buffer = io.BytesIO()
+    with zipfile.ZipFile(zip_buffer, mode="w", compression=zipfile.ZIP_DEFLATED) as zip_file:
+        for i, image in enumerate(images):
+            file_name=names[i].split('.')[0]
+            image_bytes = io.BytesIO()
+            image.save(image_bytes, format="PNG")
+            image_bytes = image_bytes.getvalue()
+            zip_file.writestr(f"images/{file_name}.png", image_bytes)
+        for i, mask in enumerate(masks):
+            file_name=names[i].split('.')[0]
+            mask_bytes = io.BytesIO()
+            mask.save(mask_bytes, format="PNG")
+            mask_bytes = mask_bytes.getvalue()
+            zip_file.writestr(f"masks/{file_name}_segmented.png", mask_bytes)
+    
+    zip_buffer.seek(0)
+    
+    # Create a download link for the zip file
+    st.download_button(
+        label="Download Images and Masks",
+        data=zip_buffer,
+        file_name="images_and_masks.zip",
+        mime="application/zip"
+    )
+
+# import streamlit as st
+# from streamlit_image_comparison import image_comparison
+# from PIL import Image
+# import io
+
+# def resize_image(image, target_size):
+#     """Resize the image to match the target size while maintaining the aspect ratio."""
+#     return image.resize(target_size, Image.BILINEAR)
+
+# def final_display(uploaded_files, segmented_files):
+#     cols = st.columns(3)
+#     with cols[0]:
+#         st.header("Original Images")
+#     with cols[1]:
+#         st.header("Segmentation Mask")
+#     with cols[2]:
+#         st.header("Compare images")
+#     for uploaded_file, segmented_file in zip(uploaded_files, segmented_files):
+#         try:
+#             # Display original image in the first column
+#             with cols[0]:
+#                 image_data = uploaded_file.read()
+#                 img = Image.open(io.BytesIO(image_data))
+#                 st.image(img, caption=uploaded_file.name, use_column_width=True)
+#         except Exception as e:
+#             st.error(f"Error processing {uploaded_file.name}: {e}")
+
+#         try:
+#             # Display masked image in the second column
+#             with cols[1]:
+#                 mask_data = segmented_file.read()
+#                 mask = Image.open(io.BytesIO(mask_data))
+#                 st.image(mask, caption=segmented_file.name, use_column_width=True)
+#         except Exception as e:
+#             st.error(f"Error processing {segmented_file.name}: {e}")
+
+#         # Display image comparison in the third column
+#         with cols[2]:
+#             # Resize original and masked images to a fixed size for comparison
+#             img_resized = resize_image(img, (800, 800))
+#             mask_resized = resize_image(mask, (800, 800))
+#             image_comparison(
+#                 img1=img_resized,
+#                 img2=mask_resized,
+#                 label1="Original",
+#                 label2="Masked",
+#                 width = 225
+#             )
+
+# def main():
+#     uploaded_files = st.file_uploader("Upload image(s) for segmentation here:", type=["jpg", "png"], accept_multiple_files=True)
+#     segmented_files = st.file_uploader("Upload segmented image(s) here:", type=["jpg", "png"], accept_multiple_files=True)
+#     if uploaded_files and segmented_files:
+#         final_display(uploaded_files, segmented_files)
+
+# if _name_ == '_main_':
+#     main()
